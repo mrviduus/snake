56f367e592ba33c9f3b9c340b88b9719f5a7272c
Vasyl <mrviduus@gmail.com>
2024-07-20 22:59:39 -0400

    add game

---
+++ Enums/Direction.cs
@@ -0,0 +1,9 @@
+namespace  Snake.Enums;
+
+public enum Direction
+{
+	Up = 0,
+	Down = 1,
+	Left = 2,
+	Right = 3
+}
---
+++ Enums/Tile.cs
@@ -0,0 +1,7 @@
+namespace Snake.Enums;
+public enum Tile
+{
+	Open = 0,
+	Snake,
+	Food,
+}
